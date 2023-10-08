dcc282e645fca4f20245188ddacda79a7510247f
suri0000 <yesle2005@naver.com>
2023-10-08 21:30:59 +0900

    [FIX] Preview error resolve!!

---
+++ Projects/Feature/Scenes/SettingScene/SettingView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingView.swift
+//  Feature
+//
+//  Created by 예슬 on 2023/10/08.
+//  Copyright © 2023 com.creative8. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 20)
+        }
+    }
+}
+
+#Preview {
+    SettingView()
+}
