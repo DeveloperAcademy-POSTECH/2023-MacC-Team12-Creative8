6534dfecc498ba894a98035ed19a1b82e14377a6
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-10-10 01:54:11 +0900

    [Feat]#1 - API networking module

---
+++ Projects/Core/DataService.swift
@@ -0,0 +1,152 @@
+//
+//  DataService.swift
+//  Core
+//
+//  Created by 고혜지 on 10/9/23.
+//  Copyright © 2023 com.creative8. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let shared = DataService()
+    
+    func searchArtistsFromMusicBrainz(artistName: String, completion: @escaping (ArtistListModel?) -> Void) {
+        if let url = URL(string: "https://musicbrainz.org/ws/2/artist?query=\(artistName)&fmt=json") {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("API_KEY", forHTTPHeaderField: "Authorization")
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print(error)
+                    completion(nil)
+                    return
+                }
+                
+                if let JSONdata = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let decodedData = try decoder.decode(ArtistListModel.self, from: JSONdata)
+                        completion(decodedData)
+                    } catch let error {
+                        print(error)
+                        completion(nil)
+                    }
+                } else {
+                    completion(nil)
+                }
+            }
+            task.resume()
+        } else {
+            completion(nil)
+        }
+    }
+    
+    func fetchSetlistsFromSetlistFM(artistMbid: String, page: Int, completion: @escaping (SetlistListModel?) -> Void) {
+        if let url = URL(string: "https://api.setlist.fm/rest/1.0/search/setlists?artistMbid=\(artistMbid)&p=\(page)") {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("Etp_bKUUaREyYBjbLpdkritldxrwWRhrw48H", forHTTPHeaderField: "x-api-key")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            request.setValue("en", forHTTPHeaderField: "Accept-Language")
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print(error)
+                    completion(nil)
+                    return
+                }
+                
+                if let JSONdata = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let decodedData = try decoder.decode(SetlistListModel.self, from: JSONdata)
+                        completion(decodedData)
+                    } catch let error {
+                        print(error)
+                        completion(nil)
+                    }
+                } else {
+                    completion(nil)
+                }
+            }
+            task.resume()
+        } else {
+            completion(nil)
+        }
+    }
+    
+    func searchArtistFromGenius(artistName: String, completion: @escaping (GeniusArtistsModel?) -> Void) {
+        if let url = URL(string: "https://api.genius.com/search?q=\(artistName)") {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("Bearer 7UINyw6MHKzr-CYFgeuL3ViZnpCLNvU1GeAo0ZKFEfyrvbyfSU5cWYMazDCXwIfh", forHTTPHeaderField: "Authorization")
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print(error)
+                    completion(nil)
+                    return
+                }
+                
+                if let JSONdata = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let decodedData = try decoder.decode(GeniusArtistsModel.self, from: JSONdata)
+                        completion(decodedData)
+                    } catch let error {
+                        print(error)
+                        completion(nil)
+                    }
+                } else {
+                    completion(nil)
+                }
+            }
+            task.resume()
+        } else {
+            completion(nil)
+        }
+    }
+
+    func fetchSongsFromGenius(artistId: Int, page: Int, completion: @escaping (GeniusSongsModel?) -> Void) {
+        if let url = URL(string: "https://api.genius.com/artists/\(artistId)/songs?page=\(page)&per_page=50") {
+            print("@LOG request url: \(url)")
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("Bearer 7UINyw6MHKzr-CYFgeuL3ViZnpCLNvU1GeAo0ZKFEfyrvbyfSU5cWYMazDCXwIfh", forHTTPHeaderField: "Authorization")
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    print(error)
+                    completion(nil)
+                    return
+                }
+                
+                if let JSONdata = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let decodedData = try decoder.decode(GeniusSongsModel.self, from: JSONdata)
+                        completion(decodedData) // 데이터 성공적으로 파싱되면 completion 핸들러 호출
+                    } catch let error {
+                        print(error)
+                        completion(nil)
+                    }
+                } else {
+                    completion(nil)
+                }
+            }
+            task.resume()
+        } else {
+            completion(nil)
+        }
+    }
+}
