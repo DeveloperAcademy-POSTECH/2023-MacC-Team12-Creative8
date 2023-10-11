d44f82f371e176d49935b671945f42332ba6a213
유인호 <yuiop1029@naver.com>
2023-10-11 21:30:52 +0900

    Revert "[ADD]#2 - add SearchView init"
    This reverts commit 3fbb7e2fe1c995a1ca37af6c2f6401079e869c0f.

--- Projects/Feature/Scenes/SearchScene/SearchView.swift
+++ Projects/Feature/Scenes/SearchScene/SearchView.swift
@@ -6,11 +6,4 @@
 //  Copyright © 2023 com.creative8. All rights reserved.
 //
 
-import SwiftUI
-
-public struct SearchView: View {
-  public init() {}
-  public var body: some View {
-    Text("Hello, World!")
-  }
-}
+import Foundation
