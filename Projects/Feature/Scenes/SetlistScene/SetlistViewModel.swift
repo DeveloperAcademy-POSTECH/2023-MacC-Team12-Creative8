71357971c2d03f96c5295af0aef8e54435873795
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-10-15 19:40:45 +0900

    [Fix]#10 - dummy data

--- Projects/Feature/Scenes/SetlistScene/SetlistViewModel.swift
+++ Projects/Feature/Scenes/SetlistScene/SetlistViewModel.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+// MARK: Core에 있는 class! 나중에 삭제할 예정!
+class KoreanTitleConverter {
+    static let shared = KoreanTitleConverter()
+    
+    func convertTitleToKorean(title: String, songList: [(String, String?)]) -> String? {
+        for song in songList {
+            if title.lowercased() == song.0.lowercased() {
+                return title
+            } else if title.lowercased() == song.1?.lowercased() {
+                return song.0
+            }
+        }
+        return nil
+    }
+}
