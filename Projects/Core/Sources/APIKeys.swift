d13b0626da9897b3754ea37597f6a9730a97c305
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-10-15 16:34:35 +0900

    [Chore]#1 - hide api keys

--- Projects/Core/Sources/APIKeys.swift
+++ Projects/Core/Sources/APIKeys.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct APIKeys {
-    let setlistFM = "Etp_bKUUaREyYBjbLpdkritldxrwWRhrw48H"
-    let musicBrainz = "API_KEY"
-    let genius = "Bearer 7UINyw6MHKzr-CYFgeuL3ViZnpCLNvU1GeAo0ZKFEfyrvbyfSU5cWYMazDCXwIfh"
+    let setlistFM = ""
+    let musicBrainz = ""
+    let genius = ""
 }
