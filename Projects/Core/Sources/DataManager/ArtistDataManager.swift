770dfad586002c9cdb2b1b37f7d0db4ea2eab531
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-10-18 22:38:33 +0900

    [Fix]#21 - get artist data from genius

--- Projects/Core/Sources/DataManager/ArtistDataManager.swift
+++ Projects/Core/Sources/DataManager/ArtistDataManager.swift
@@ -7,3 +7,127 @@
 //
 
 import Foundation
+
+public final class ArtistDataManager {
+  static let shared = ArtistDataManager()
+  
+  let dataService: DataService = DataService.shared
+  
+  public func getArtistInfo(artistName: String, artistAlias: String, completion: @escaping (ArtistInfo?) -> Void) {
+    var parsedSongList: [(String, String?)] = []
+    var artistInfo: ArtistInfo?
+    var songList: [String] = []
+    
+    dataService.searchArtistFromGenius(artistName: artistName) { result in
+      if let result = result {
+        DispatchQueue.main.async {
+          artistInfo = self.findArtistIdAndImage(artistName: artistName, artistAlias: artistAlias, hits: result.response?.hits ?? [])
+          
+          self.fetchAllSongs(artistId: artistInfo?.gid ?? 0) { result in
+            if let result = result {
+              songList = result
+              for song in songList {
+                parsedSongList.append(
+                  (self.extractTextBeforeParentheses(from: song),
+                   self.extractTextInsideFirstParentheses(from: song))
+                )
+              }
+              artistInfo?.songList = parsedSongList
+              completion(artistInfo)
+            } else {
+              completion(nil)
+            }
+          }
+          completion(artistInfo)
+          
+        }
+      } else {
+        completion(nil)
+      }
+    }
+  }
+  
+  private func fetchAllSongs(artistId: Int, completion: @escaping ([String]?) -> Void) {
+    var songList: [String] = []
+    
+    func fetchPage(page: Int) {
+      dataService.fetchSongsFromGenius(artistId: artistId, page: page) { result in
+        guard let result = result, let songs = result.response?.songs else {
+          completion(nil)
+          return
+        }
+        for song in songs {
+          songList.append(song.title ?? "")
+        }
+        if let nextPage = result.response?.nextPage {
+          fetchPage(page: nextPage)
+        } else {
+          completion(songList)
+        }
+      }
+    }
+    
+    fetchPage(page: 1)
+  }
+  
+  private func findArtistIdAndImage(artistName: String, artistAlias: String, hits: [Hit]) -> ArtistInfo? {
+    for hit in hits {
+      if let name = hit.result?.primaryArtist?.name {
+        let filteredName = stringFilter(name)
+        let filteredArtistName = stringFilter(artistName)
+        let filteredArtistAlias = stringFilter(artistAlias)
+        
+        if removeFirstParentheses(from: filteredName) == filteredArtistName ||
+            extractTextInsideFirstParentheses(from: filteredName) == filteredArtistName ||
+            removeFirstParentheses(from: filteredName) == filteredArtistAlias ||
+            extractTextInsideFirstParentheses(from: filteredName) == filteredArtistAlias {
+          print("FIND ARTIST: \(name)")
+          return (ArtistInfo(gid: hit.result?.primaryArtist?.id, imageUrl: hit.result?.primaryArtist?.imageURL, songList: nil))
+        }
+      }
+    }
+    print("FAILED TO FIND ARTIST")
+    return nil
+  }
+  
+  private func stringFilter(_ str: String) -> String {
+    return str
+      .lowercased()
+      .trimmingCharacters(in: .whitespaces)
+      .replacingOccurrences(of: " ", with: "")
+      .filter { $0.unicodeScalars.first?.value != 8203 }
+  }
+  
+  // 첫 번째 괄호 이전의 텍스트를 추출
+  private func extractTextBeforeParentheses(from input: String) -> String {
+    if let range = input.range(of: "(") {
+      let textBeforeParentheses = input[..<range.lowerBound].trimmingCharacters(in: .whitespacesAndNewlines)
+      return String(textBeforeParentheses)
+    } else {
+      return input
+    }
+  }
+  
+  // 첫 번째 괄호 사이의 텍스트를 추출
+  private func extractTextInsideFirstParentheses(from input: String) -> String? {
+    if let startIndex = input.firstIndex(of: "("), let endIndex = input.firstIndex(of: ")"), startIndex < endIndex {
+      let range = (input.index(after: startIndex)..<endIndex)
+      let textInsideParentheses = input[range].trimmingCharacters(in: .whitespacesAndNewlines)
+      return String(textInsideParentheses)
+    } else {
+      return nil
+    }
+  }
+  
+  //  첫 번째 괄호를 제거
+  private func removeFirstParentheses(from input: String) -> String {
+    if let startIndex = input.firstIndex(of: "("), let endIndex = input.firstIndex(of: ")"), startIndex < endIndex {
+      let firstParenthesesRange = startIndex...endIndex
+      let modifiedString = input.replacingCharacters(in: firstParenthesesRange, with: "")
+      return modifiedString.trimmingCharacters(in: .whitespacesAndNewlines)
+    } else {
+      return input
+    }
+  }
+  
+}
