47609b5d2ff7ada7c4a8b01efb4dc9586085c748
유인호 <yuiop1029@naver.com>
2023-10-24 06:21:59 +0900

    #65[FIX]: Fix

--- Projects/Feature/Scenes/ArchiveScene/Component/Artist/SeeAllArtist.swift
+++ Projects/Feature/Scenes/ArchiveScene/Component/Artist/SeeAllArtist.swift
@@ -12,6 +12,8 @@ import Core
 
 struct SeeAllArtist: View {
   @Query var likeArtist: [LikeArtist]
+  @StateObject var dataManager = SwiftDataManager()
+  @Environment(\.modelContext) var modelContext
   var body: some View {
     ScrollView {
       ForEach(likeArtist) { item in
@@ -19,15 +21,19 @@ struct SeeAllArtist: View {
           ArchiveArtistCell(artistUrl: URL(string: item.artistInfo.imageUrl)!, isNewUpdate: false)
           Text("\(item.artistInfo.name)")
           Spacer()
-          Button {
-              
+          Menu {
+            NavigationLink("아티스트로 가기") { ArtistView(artistName: item.artistInfo.name, artistAlias: item.artistInfo.alias, artistMbid: item.artistInfo.mbid) }
+            Button("좋아요 취소") { dataManager.deleteLikeArtist(item) }
           } label: {
             Image(systemName: "ellipsis")
               .foregroundStyle(.black)
               .rotationEffect(.degrees(-90))
+              .padding(.horizontal)
+              .background(Color.clear)
           }
         }
       }
+      .onAppear { dataManager.modelContext = modelContext }
     }
     .padding()
     .navigationTitle("좋아요한 아티스트")
