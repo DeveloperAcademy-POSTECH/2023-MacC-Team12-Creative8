dc941788e65d7377463d5edbe12f431060b4ee57
HyoWon Choi <eve652358@gmail.com>
2023-10-23 13:02:44 +0900

    [Feat]#15 add TabBarView

---
+++ Projects/Feature/Scenes/MainScene/View/TabBarView.swift
@@ -0,0 +1,53 @@
+//
+//  TabBarView.swift
+//  Feature
+//
+//  Created by 최효원 on 10/23/23.
+//  Copyright © 2023 com.creative8.seta. All rights reserved.
+//
+
+import SwiftUI
+
+public struct TabBarView: View {
+  @State private var selectedTab: Tab = .home
+  
+  enum Tab {
+    case home
+    case search
+    case archiving
+    case setting
+  }
+  
+  public init() {
+  }
+  
+  public var body: some View {
+    TabView(selection: $selectedTab) {
+      MainView()
+        .tabItem {
+          Label("세트리스트", systemImage: "music.note.house.fill")
+        }
+        .tag(Tab.home)
+      
+      SearchView()
+        .tabItem {
+          Label("검색", systemImage: "magnifyingglass")
+        }
+        .tag(Tab.search)
+      
+      ArchivingView()
+        .tabItem {
+          Label("검색", systemImage: "heart.fill")
+        }
+        .tag(Tab.archiving)
+      
+      SettingView()
+        .tabItem {
+          Label("더보기", systemImage: "ellipsis")
+        }
+        .tag(Tab.setting)
+    }
+    .accentColor(.blue)
+    .navigationBarBackButtonHidden(true)
+  }
+}
