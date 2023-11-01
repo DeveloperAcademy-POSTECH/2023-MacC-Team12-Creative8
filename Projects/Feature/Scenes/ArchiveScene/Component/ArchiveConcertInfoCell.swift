99ca2c4d365ebd96a57850dc2d0c6fba186b2659
유인호 <yuiop1029@naver.com>
2023-11-01 14:48:16 +0900

    df

---
+++ Projects/Feature/Scenes/ArchiveScene/Component/ArchiveConcertInfoCell.swift
@@ -0,0 +1,19 @@
+//
+//  ArchiveConcertInfoCell.swift
+//  Feature
+//
+//  Created by A_Mcflurry on 10/30/23.
+//  Copyright © 2023 com.creative8.seta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ArchiveConcertInfoCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchiveConcertInfoCell()
+}
---
+++ Projects/Feature/Scenes/ArchiveScene/Component/ArtistSetCell.swift
@@ -0,0 +1,9 @@
+//
+//  ArtistSetCell.swift
+//  Feature
+//
+//  Created by A_Mcflurry on 10/31/23.
+//  Copyright © 2023 com.creative8.seta. All rights reserved.
+//
+
+import Foundation
