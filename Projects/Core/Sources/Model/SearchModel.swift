54ad6ece62ee841764358b011b78c869ee744ddd
HyoWon Choi <eve652358@gmail.com>
2023-11-08 04:34:47 +0900

    [Fix] modify searchModel

--- Projects/Core/Sources/Model/SearchModel.swift
+++ Projects/Core/Sources/Model/SearchModel.swift
@@ -9,30 +9,30 @@
 import Foundation
 
 public let koreanArtistModel: [ArtistInfo] = [
-  ArtistInfo(name: "BTS", mbid: "0d79fe8e-ba27-4859-bb8c-2f255f346853", imageUrl: "https://images.genius.com/07ac668b1493f2de64ec854f5b20361e.800x800x1.jpg"),
-    ArtistInfo(name: "NewJeans", mbid: "49204a7a-ed85-407a-828f-6fd46f1d8126", imageUrl: "https://images.genius.com/0bfc08c184e0f7420adcac54c3bbf34d.640x640x1.jpg"),
-    ArtistInfo(name: "JayPark", mbid: "3dda8202-ce15-4031-862a-77bc6759d15e", imageUrl: "https://images.genius.com/490bc306972ca7572fc52e8ac4a22d96.1000x1000x1.jpg"),
-    ArtistInfo(name: "로꼬", mbid: "9e9e2a33-905c-4436-8ad8-8d9df00ba23d", imageUrl: "https://images.genius.com/2b1511e9d2cce0ac46041a19602c7ce7.395x395x1.jpg"),
-    ArtistInfo(name: "카더가든", mbid: "9f2862c0-22a9-4fd2-9f1c-07e17f356137", imageUrl: "https://images.genius.com/db6fa8e83789a613d2f069671c9e3f6e.1000x1000x1.jpg"),
-    ArtistInfo(name: "Paul Kim", mbid: "8968d1ad-c830-4ab2-92d2-bd6bea0b6f42", imageUrl: "https://images.genius.com/47abf7d9653ee61a9547ebd41b1d967e.654x654x1.jpg"),
-    ArtistInfo(name: "StrayKids", mbid: "142b343d-bf5a-428c-a64f-6d1a7566bbe9", imageUrl: "https://images.genius.com/5eb3e5c18c3dd0a1a254057c4315f17e.1000x1000x1.jpg"),
-    ArtistInfo(name: "NCT DREAM", mbid: "dc7c8277-d4b6-4bca-bdfe-50eee42536ac",  imageUrl: "https://images.genius.com/7c4340c13d532c46cd42a5eea7f0ee95.800x800x1.jpg"),
-    ArtistInfo(name: "Woodz", mbid: "25c73c37-9517-4144-8ef6-32541e82ff42", imageUrl: "https://images.genius.com/7bb7e0d5d7ee4b2e1031d11a6c035675.1000x1000x1.jpg"),
-    ArtistInfo(name: "이영지", mbid: "0433e75d-70b6-4118-9ff8-9b6f7e534093", imageUrl: "https://images.genius.com/435d38c3ca958671899a59ca3fd03ce5.600x600x1.jpg"),
-    ArtistInfo(name: "윤하", mbid: "5e3bc4c7-adbe-40e0-b56e-57d755908d52", imageUrl: "https://images.genius.com/319b2fc2658714d42e216bbafc4c1152.960x960x1.jpg")
+  ArtistInfo(name: "BTS", mbid: "0d79fe8e-ba27-4859-bb8c-2f255f346853", imageUrl: "BTS"),
+    ArtistInfo(name: "NewJeans", mbid: "49204a7a-ed85-407a-828f-6fd46f1d8126", imageUrl: "NewJeans"),
+    ArtistInfo(name: "JayPark", mbid: "3dda8202-ce15-4031-862a-77bc6759d15e", imageUrl: "JayPark"),
+    ArtistInfo(name: "로꼬", mbid: "9e9e2a33-905c-4436-8ad8-8d9df00ba23d", imageUrl: "Loco"),
+    ArtistInfo(name: "카더가든", mbid: "9f2862c0-22a9-4fd2-9f1c-07e17f356137", imageUrl: "CarTheGarden"),
+    ArtistInfo(name: "Paul Kim", mbid: "8968d1ad-c830-4ab2-92d2-bd6bea0b6f42", imageUrl: "PaulKim"),
+    ArtistInfo(name: "StrayKids", mbid: "142b343d-bf5a-428c-a64f-6d1a7566bbe9", imageUrl: "StrayKids"),
+    ArtistInfo(name: "NCT DREAM", mbid: "dc7c8277-d4b6-4bca-bdfe-50eee42536ac",  imageUrl: "NCTDream"),
+    ArtistInfo(name: "Woodz", mbid: "25c73c37-9517-4144-8ef6-32541e82ff42", imageUrl: "Woodz"),
+    ArtistInfo(name: "이영지", mbid: "0433e75d-70b6-4118-9ff8-9b6f7e534093", imageUrl: "LeeYoungJi"),
+    ArtistInfo(name: "윤하", mbid: "5e3bc4c7-adbe-40e0-b56e-57d755908d52", imageUrl: "Yoonha")
 ]
 
 public let foreignArtistModel: [ArtistInfo] = [
-  ArtistInfo(name: "Bruno Mars", mbid: "afb680f2-b6eb-4cd7-a70b-a63b25c763d5", imageUrl: "https://images.genius.com/ec38c902389e5d9df5782aa54fcf3c00.888x888x1.jpg"),
-  ArtistInfo(name: "The Weekend", mbid: "c8b03190-306c-4120-bb0b-6f2ebfc06ea9", imageUrl: "https://images.genius.com/1bab7f9dbd1216febc16d73ae4da9bd0.1000x1000x1.jpg"),
-  ArtistInfo(name: "Maroon5", mbid: "0ab49580-c84f-44d4-875f-d83760ea2cfe", imageUrl: "https://images.genius.com/305fdc0dbaf0c5eb8cfe7b0b8d1cabdd.720x720x1.jpg"),
-  ArtistInfo(name: "Post Malone", mbid: "b1e26560-60e5-4236-bbdb-9aa5a8d5ee19", imageUrl: "https://images.genius.com/7a94007b6d7ae5f57160770dc5f3a2d2.623x623x1.jpg"),
-  ArtistInfo(name: "Sam Smith", mbid: "5a85c140-dcf9-4dd2-b2c8-aff0471549f3", imageUrl: "https://images.genius.com/ac293597db73450fa6ee4be5bc74e65a.419x419x1.jpg"),
-  ArtistInfo(name: "Doja Cat", mbid: "5df62a88-cac9-490a-b62c-c7c88f4020f4", imageUrl: "https://images.genius.com/0a8981bfd6fb08e0310b2cf1dd593a41.900x900x1.jpg"),
-  ArtistInfo(name: "Eminem", mbid: "b95ce3ff-3d05-4e87-9e01-c97b66af13d4", imageUrl: "https://images.genius.com/76c536a17ca35f7edd1f78e129609fe0.573x573x1.jpg"),
-  ArtistInfo(name: "Dua Lipa", mbid: "6f1a58bf-9b1b-49cf-a44a-6cefad7ae04f", imageUrl: "https://images.genius.com/6dd7902577d2e9ac60969524f432e007.966x966x1.jpg"),
-  ArtistInfo(name: "John K", mbid: "be465d46-9615-4722-b828-ea531ff97ea3", imageUrl: "https://images.genius.com/92ea0895d276545f2a74d4e4b5ce0c74.1000x1000x1.jpg"),
-  ArtistInfo(name: "Troye Sivan", mbid: "e5712ceb-c37a-4c49-a11c-ccf4e21852d4", imageUrl: "https://images.genius.com/a7d44a7ad8fbfaabc8726e4777abc421.400x400x1.jpg"),
-  ArtistInfo(name: "Drake", mbid: "9fff2f8a-21e6-47de-a2b8-7f449929d43f", imageUrl: "https://images.genius.com/b428ba956c42c7e1bb1d56e729702724.486x486x1.png")
+  ArtistInfo(name: "Bruno Mars", mbid: "afb680f2-b6eb-4cd7-a70b-a63b25c763d5", imageUrl: "BrunoMars"),
+  ArtistInfo(name: "The Weekend", mbid: "c8b03190-306c-4120-bb0b-6f2ebfc06ea9", imageUrl: "TheWeekend"),
+  ArtistInfo(name: "Maroon5", mbid: "0ab49580-c84f-44d4-875f-d83760ea2cfe", imageUrl: "Maroon5"),
+  ArtistInfo(name: "Post Malone", mbid: "b1e26560-60e5-4236-bbdb-9aa5a8d5ee19", imageUrl: "PostMalone"),
+  ArtistInfo(name: "Sam Smith", mbid: "5a85c140-dcf9-4dd2-b2c8-aff0471549f3", imageUrl: "SamSmith"),
+  ArtistInfo(name: "Doja Cat", mbid: "5df62a88-cac9-490a-b62c-c7c88f4020f4", imageUrl: "DojaCat"),
+  ArtistInfo(name: "Eminem", mbid: "b95ce3ff-3d05-4e87-9e01-c97b66af13d4", imageUrl: "Eminem"),
+  ArtistInfo(name: "Dua Lipa", mbid: "6f1a58bf-9b1b-49cf-a44a-6cefad7ae04f", imageUrl: "DuaLipa"),
+  ArtistInfo(name: "John K", mbid: "be465d46-9615-4722-b828-ea531ff97ea3", imageUrl: "JohnK"),
+  ArtistInfo(name: "Troye Sivan", mbid: "e5712ceb-c37a-4c49-a11c-ccf4e21852d4", imageUrl: "TroyeSivan"),
+  ArtistInfo(name: "Drake", mbid: "9fff2f8a-21e6-47de-a2b8-7f449929d43f", imageUrl: "Drake")
   
 ]
