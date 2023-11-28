1c55f9b778245036892bf945b1e2a64e385b7882
Lorenzo <tnalsdlekgsk1@icloud.com>
2023-11-29 00:26:36 +0900

    [Feat]#169 - 파일 수정 완료

--- Projects/Core/Sources/Service/APIKeys.swift
+++ Projects/Core/Sources/Service/APIKeys.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct APIKeys {
-    let setlistFM = "K7QlHJ0nE6Ih48FX9wQfbo-6uFAq083uTU6g"
+    let setlistFM = "Etp_bKUUaREyYBjbLpdkritldxrwWRhrw48H"
     let musicBrainz = "API_KEY"
     let genius = "Bearer 7UINyw6MHKzr-CYFgeuL3ViZnpCLNvU1GeAo0ZKFEfyrvbyfSU5cWYMazDCXwIfh"
 }
--- Projects/Feature/Scenes/SettingScene/View/AskView.swift
+++ Projects/Feature/Scenes/SettingScene/View/AskView.swift
@@ -11,7 +11,7 @@ import MessageUI
 import UI
 
 struct AskView: View {
-  
+  @State var isMailErrorAlertPresented: Bool = false
   @ObservedObject var coordinator = Coordinator()
   
   var body: some View {
@@ -20,7 +20,24 @@ struct AskView: View {
     }, label: {
       LinkLabelView(linkLabel: "문의하기")
     })
+    .alert(isPresented: $isMailErrorAlertPresented) {
+        Alert(
+            title: Text("메일 전송 실패"),
+            message: Text("메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요."),
+            primaryButton: .default(Text("App Store로 이동하기")) {
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            },
+            secondaryButton: .destructive(Text("취소"))
+        )
+    }
   }
+  
   
   func commentsButtonTapped() {
     guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
@@ -35,12 +52,13 @@ struct AskView: View {
       let bodyString =
       """
       안녕하세요. Seta 입니다. 아래 내용을 보내주시면 문의 확인에 도움이 됩니다.
+      Hello, this is Seta. Providing the following information will help us investigate your inquiry
       
-        - iOS 버전 :
-        - 기기 모델명 :
-        - 문제발생일시 :
-        - 문의 내용 :
-      
+        - iOS 버전(iOS version) :
+        - 기기 모델명(Device model) :
+        - 문제발생일시(Date and time of issue) :
+        - 문의 내용(Details of your inquiry) :
+
       
       """
       
@@ -56,26 +74,27 @@ struct AskView: View {
         rootViewController.present(composeViewController, animated: true, completion: nil)
       }
     } else {
-      print("메일 보내기 실패")
-      let sendMailErrorAlert = UIAlertController(
-        title: "메일 전송 실패",
-        message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.",
-        preferredStyle: .alert)
-      let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
-        if let url = URL(
-          string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
-          if #available(iOS 10.0, *) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-          } else {
-            UIApplication.shared.openURL(url)
-          }
-        }
-      }
-      let cancelAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
-      
-      sendMailErrorAlert.addAction(goAppStoreAction)
-      sendMailErrorAlert.addAction(cancelAction)
-      rootViewController.present(sendMailErrorAlert, animated: true, completion: nil)
+//      print("메일 보내기 실패")
+//      let sendMailErrorAlert = UIAlertController(
+//        title: "메일 전송 실패",
+//        message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.",
+//        preferredStyle: .alert)
+//      let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+//        if let url = URL(
+//          string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+//          if #available(iOS 10.0, *) {
+//            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//          } else {
+//            UIApplication.shared.openURL(url)
+//          }
+//        }
+//      }
+//      let cancelAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+//      
+//      sendMailErrorAlert.addAction(goAppStoreAction)
+//      sendMailErrorAlert.addAction(cancelAction)
+//      rootViewController.present(sendMailErrorAlert, animated: true, completion: nil)
+      isMailErrorAlertPresented.toggle()
     }
   }
   
