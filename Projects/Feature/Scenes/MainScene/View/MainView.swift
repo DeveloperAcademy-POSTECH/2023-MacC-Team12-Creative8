a9e4e5a68a8fed582d9e703997906ed86f8f8d62
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-11-29 08:09:01 +0900

    [Fix] - function duplicate

--- Projects/Feature/Scenes/MainScene/View/MainView.swift
+++ Projects/Feature/Scenes/MainScene/View/MainView.swift
@@ -145,20 +145,6 @@ public struct MainView: View {
     .safeAreaPadding(.trailing, UIWidth * 0.005)
   }
 }
-extension View {
-  func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-    clipShape(RoundedCorner(radius: radius, corners: corners))
-  }
-}
-struct RoundedCorner: Shape {
-  var radius: CGFloat = .infinity
-  var corners: UIRectCorner = .allCorners
-  
-  func path(in rect: CGRect) -> Path {
-    let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-    return Path(path.cgPath)
-  }
-}
 
 #Preview {
   MainView(selectedTab: .constant(.home), viewModel: MainViewModel(), tabViewManager: .init(consecutiveTaps: Empty().eraseToAnyPublisher()))
