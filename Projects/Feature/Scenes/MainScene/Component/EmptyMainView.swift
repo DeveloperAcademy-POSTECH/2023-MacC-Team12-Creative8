991f9dbc11c96e1a81cb369f02948d2bc2b2de87
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-03-05 17:00:33 +0900

    [fix]#192 - 배경색 수정

--- Projects/Feature/Scenes/MainScene/Component/EmptyMainView.swift
+++ Projects/Feature/Scenes/MainScene/Component/EmptyMainView.swift
@@ -16,38 +16,41 @@ struct EmptyMainView: View {
   @Binding var selectedTab: Tab
   
   var body: some View {
-    VStack {
-      Spacer()
-      Text("찜한 아티스트가 없습니다")
-        .font(.callout)
-        .padding(.bottom)
-        .foregroundStyle(Color.mainBlack)
-      Group {
-        VStack {
-          Text("관심있는 아티스트 정보를 빠르게")
-          Text("확인하고 싶으시다면 찜을 해주세요")
+    ZStack {
+      Color.backgroundWhite
+      VStack {
+        Spacer()
+        Text("찜한 아티스트가 없습니다")
+          .font(.callout)
+          .padding(.bottom)
+          .foregroundStyle(Color.mainBlack)
+        Group {
+          VStack {
+            Text("관심있는 아티스트 정보를 빠르게")
+            Text("확인하고 싶으시다면 찜을 해주세요")
+          }
         }
-      }
         .font(.footnote)
         .multilineTextAlignment(.center)
         .foregroundStyle(Color.fontGrey2)
         .padding(.bottom)
-      Button {
-        selectedTab = .search
-      } label: {
-        HStack {
-          Text("아티스트 찜하러 가기")
-          Image(systemName: "magnifyingglass")
+        Button {
+          selectedTab = .search
+        } label: {
+          HStack {
+            Text("아티스트 찜하러 가기")
+            Image(systemName: "magnifyingglass")
+          }
+          .bold()
+          .foregroundStyle(Color.mainWhite)
+          .font(.system(size: 14))
+          .padding(EdgeInsets(top: 17, leading: 22, bottom: 17, trailing: 22))
+          .background(RoundedRectangle(cornerRadius: 14)
+            .foregroundStyle(Color.buttonBlack))
         }
-        .bold()
-        .foregroundStyle(Color.mainWhite)
-        .font(.system(size: 14))
-        .padding(EdgeInsets(top: 17, leading: 22, bottom: 17, trailing: 22))
-        .background(RoundedRectangle(cornerRadius: 14)
-          .foregroundStyle(Color.buttonBlack))
+        .padding(.vertical)
+        Spacer()
       }
-      .padding(.vertical)
-      Spacer()
     }
   }
 }
