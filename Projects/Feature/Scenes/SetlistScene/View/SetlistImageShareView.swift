680c17acee83737d92ac3be86c187c1137285be6
suri0000 <yesle2005@naver.com>
2024-05-12 22:18:13 +0900

    [Feat]#205-Add SetlistImageShareView

---
+++ Projects/Feature/Scenes/SetlistScene/View/SetlistImageShareView.swift
@@ -0,0 +1,19 @@
+//
+//  SetlistImageShareView.swift
+//  Feature
+//
+//  Created by 예슬 on 5/12/24.
+//  Copyright © 2024 com.creative8.seta. All rights reserved.
+//
+
+import SwiftUI
+
+struct SetlistImageShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetlistImageShareView()
+}
