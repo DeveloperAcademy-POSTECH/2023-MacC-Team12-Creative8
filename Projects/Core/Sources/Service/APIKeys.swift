4eaa0bef9ef02ffbc1a0f24c5dfb31f8d7bb2702
유인호 <yuiop1029@naver.com>
2024-07-14 22:57:29 +0900

    [FIX]#216 - 죄송..

--- Projects/Core/Sources/Service/APIKeys.swift
+++ Projects/Core/Sources/Service/APIKeys.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 struct APIKeys {
-  let setlistFM = "K7QlHJ0nE6Ih48FX9wQfbo-6uFAq083uTU6g"
+  let setlistFM = "API_KEY"
   let musicBrainz = "API_KEY"
-  let genius = "Bearer 7UINyw6MHKzr-CYFgeuL3ViZnpCLNvU1GeAo0ZKFEfyrvbyfSU5cWYMazDCXwIfh"
-  static let spotifyClientID = "f782b60e9c154ce7b5e8619f44fffa83"
-  static let spotifyClientSecreat = "162c994ab7904779be36df3a829c94d1"
+  let genius = "API_KEY"
+  static let spotifyClientID = "API_KEY"
+  static let spotifyClientSecreat = "API_KEY"
 }
