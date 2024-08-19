3b48e87e8db32abb7437919d416e23ee50c0a004
Ko-HyeJi <hy2ji.k5@gmail.com>
2024-08-19 22:32:13 +0900

    feat: toast message blur

--- Projects/Feature/Scenes/SetlistScene/Component/ToastMessageView.swift
+++ Projects/Feature/Scenes/SetlistScene/Component/ToastMessageView.swift
@@ -34,9 +34,41 @@ struct ToastMessageView: View {
      }
      .padding(15)
      .frame(maxWidth: .infinity)
-     .background(
-       Color.toastBG
-        .cornerRadius(6.0)
-     )
+     .background(Blur().cornerRadius(6.0))
+     .background(Color.toastBG.cornerRadius(6.0))
+  }
+}
+
+open class UIBackdropView: UIView {
+
+  open override class var layerClass: AnyClass {
+    NSClassFromString("CABackdropLayer") ?? CALayer.self
+  }
+}
+
+public struct Backdrop: UIViewRepresentable {
+
+  public init() {}
+
+  public func makeUIView(context: Context) -> UIBackdropView {
+    UIBackdropView()
+  }
+
+  public func updateUIView(_ uiView: UIBackdropView, context: Context) {}
+}
+
+public struct Blur: View {
+
+  public var radius: CGFloat
+  public var opaque: Bool
+
+  public init(radius: CGFloat = 3.0, opaque: Bool = false) {
+    self.radius = radius
+    self.opaque = opaque
+  }
+
+  public var body: some View {
+    Backdrop()
+      .blur(radius: radius, opaque: opaque)
   }
 }
